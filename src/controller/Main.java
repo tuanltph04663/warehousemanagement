cc63bdffd7acc2b01f06e755754486e467056276
tuan <tuanltph04663@fpt.edu.vn>
2018-07-16 01:31:03 +0700

    init project

---
+++ src/controller/Main.java
@@ -0,0 +1,16 @@
+package controller;
+
+import javax.swing.JFrame;
+
+public class Main extends JFrame{
+
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+}
